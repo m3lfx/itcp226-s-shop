ad28bf3d94481da3c56fb8731cdd2e6ae81f0e97
rommel dalisay <rommelmdalisay@gmail.com>
2026-01-29 09:13:52 +0800

    soft delete

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ItemController extends Controller
 {
@@ -11,7 +12,9 @@ class ItemController extends Controller
      */
     public function index()
     {
-        //
+        $items = DB::table('item')->join('stock', 'item.item_id', '=', 'stock.item_id')->get();
+
+        return view('item.index', compact('items'));
     }
 
     /**
--- app/Models/Item.php
+++ app/Models/Item.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Item extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
     protected $table = 'item';
     public $timestamps = false;
     protected $primaryKey = 'item_id';
