adf736675352de1e5a21f23ae527bf7159ff977b
rommel dalisay <rommelmdalisay@gmail.com>
2026-02-05 08:45:18 +0800

    user seeder

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -172,4 +172,58 @@ class ItemController extends Controller
         Session::put('cart', $cart);
         return redirect('/')->with('success', 'item added to cart');
     }
+
+    public function postCheckout()
+    {
+
+        if (!Session::has('cart')) {
+            return redirect()->route('getCart');
+        }
+        $oldCart = Session::get('cart');
+        $cart = new Cart($oldCart);
+        // dd($cart, $cart->items);
+        try {
+            $customer = Customer::where('user_id', Auth::id())->first();
+            // dd($customer);
+            DB::beginTransaction();
+            $order = new Order();
+            // $order->customer_id = $customer->customer_id;
+            $order->customer_id = 1;
+            $order->date_placed = now();
+            $order->date_shipped = Carbon::now()->addDays(5);
+
+            $order->shipping = 10.00;
+            // $order->status = 'Processing';
+
+            // $order->save();
+            // dd($cart->items);
+
+            foreach ($cart->items as $items) {
+                $id = $items['item']['item_id'];
+                // dd($id);
+
+                DB::table('orderline')
+                    ->insert(
+                        [
+                            'item_id' => $id,
+                            'orderinfo_id' => $order->orderinfo_id,
+                            'quantity' => $items['qty']
+                        ]
+                    );
+                $stock = Stock::find($id);
+                $stock->quantity = $stock->quantity - $items['qty'];
+                $stock->save();
+            }
+            // dd($order);
+        } catch (\Exception $e) {
+            dd($e->getMessage());
+            DB::rollback();
+            // dd($order);
+            return redirect()->route('getCart')->with('error', $e->getMessage());
+        }
+
+        DB::commit();
+        Session::forget('cart');
+        return redirect('/')->with('success', 'Successfully Purchased Your Products!!!');
+    }
 }
